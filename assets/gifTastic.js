a9e52a34b0ca06fdda05f07db45a4530e2f34255
kyoukel <kimyoukel@gmail.com>
2018-03-08 15:12:11 -0800

    defined queryURL as a variable and console.log my queryURL - is working. Attempted to use $.ajax & GET method to return the response and I am getting a No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access - message.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -4,4 +4,13 @@ $('button').on('click', function(){
     // alert("Button Clicked!");
     var h = $(this).data("search");
     console.log(h);
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "api_key=dc6zaTOxFJmzC&limit=10";
+    console.log(queryURL);
+
+    $.ajax({
+        url:queryURL, method: 'GET'})
+        .done(function(response){
+            console.log(response);
+        })
 })
