4650fb3530abf0b8a87f53213d33c06c25a54499
kyoukel <kimyoukel@gmail.com>
2018-03-08 15:46:56 -0800

    strange message, but turned out it was a syntax error in my queryURL. Fixed issues.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -2,10 +2,10 @@
 
 $('button').on('click', function(){
     // alert("Button Clicked!");
-    var h = $(this).data("search");
+    var h = $(this).attr('data-search');
     console.log(h);
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "&" + "api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
     console.log(queryURL);
 
     $.ajax({
