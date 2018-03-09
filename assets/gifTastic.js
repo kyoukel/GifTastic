066a18fa1c2adc3dabf6f6b3846581ec69ec876d
kyoukel <kimyoukel@gmail.com>
2018-03-09 15:25:12 -0800

    success - buttons were created by for loop, results.length for loop is displaying the rating data and the gifs on the DOM, and the ratings are below the gifs. Working on getting all 10 gifs to appear when each hero button is clicked.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -1,66 +1,21 @@
 
 // Initial array of super heroes
-var superHeroes = ["Wonder Woman", "Hulk", "Captain America", "Superman", "Thor", "Iron Man", "Spider-Man", "Guardians of the Galaxy", "Deadpool", "Batman", "X-Men", "Ant-Man", "Black Panther", "Suicide Squad", "Flash", "Supergirl", "Green Lantern", "Daredevil", "Elektra", "Ninja Turtles"];
-
-// displayRatingInfo function re-renders the HTML to display the appropriate content
-// function displayRatingInfo() {
-
-    var h = $(this).attr('data-search');
-    console.log(this);
-    
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "&" + "api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
-    console.log(queryURL);
-    // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-        url:queryURL, 
-        method: 'GET'
-    }).then(function(response) {
-        console.log(response)
-
-        // Creating a div to hold the gifs
-        var heroDiv = $("<div class='hero'>");
-
-        // Storing the rating data
-        var rating = response.data[0].rating;
-        // console.log(rating);
-        
-        // Creating an element to have the rating displayed
-        var displayRating = $('<p>').text("Rating: " + rating);
-
-        // Display the rating
-        heroDiv.append(displayRating);
-        console.log(rating);
-
-       // Retrieving the URL for the image
-       var imgURL = response.data[0].images.downsized.url;
-       console.log(imgURL);
-
-        // Creating an element to hold the image
-        var image = $("<img>").attr("src", imgURL);
-        console.log(image);
-        
-        // Appending the image
-        heroDiv.append(image);
-
-        // // Putting the entire movie above the previous movies
-        // $("#heroes-view").prepend(heroDiv);
-    });
-// };
+var superHeroes = ["Wonder Woman", "Hulk", "Captain America", "Superman", "Thor", "Iron Man", "Spider-Man", "Guardians of the Galaxy", "Deadpool", "Batman", "X-Men", "Ant-Man", "Black Panther", "Suicide Squad", "The Flash", "Supergirl", "Green Lantern", "Daredevil", "Elektra", "She-Ra"];
 
 // Function for displaying data
 function renderButtons() {
 
-    // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    $("#heroes-view").empty();
+    // Deleting the hero buttons prior to ADDING NEW MOVIE BUTTONS
+    // (this is necessary otherwise will have repeat buttons)
+    $("#buttons-view").empty();
 
     // Looping through the array of superHeroes
     for (var i = 0; i < superHeroes.length; i++) {
 
-        // Then dynamicaly generating buttons for each movie in the array
+        // Then dynamicaly generating buttons for each hero in the array
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var b = $("<button>");
-        // Adding a class of movie-btn to our button
+        // Adding a class of hero-btn to our button
         b.addClass("hero-btn");
         // Adding a data-attribute
         b.attr("data-name", superHeroes[i]);    // *****UNSURE OF WHICH DATA-ATTR TO USE OR
@@ -73,7 +28,8 @@ function renderButtons() {
 };    
 
 // This function handles events when the submit button is clicked
-    $('#add-hero').on('click', function(event){
+    function addHero(event) {
+    // $('#add-hero').on('click', function(event){
         // alert("Button Clicked!");
 
         // event.preventDefault() prevents the form from trying to submit itself.
@@ -83,14 +39,82 @@ function renderButtons() {
         // This line will grab the text from the input box
         var hero = $("#heroes-input").val().trim();
         console.log(hero);
-        // The movie from the textbox is then added to our array
+        // The superhero entered from the textbox is then added to our array
         superHeroes.push(hero);
 
-        // calling renderButtons which handles the processing of our movie array
+        // calling renderButtons which handles the processing of our superHeroes array
         renderButtons();
-    });
+    }
     
-    renderButtons();
+// displayRatingInfo function re-renders the HTML to display the appropriate content
+function displayRatingInfo() {
+
+    var h = $(this).attr('data-name');
+    // console.log(this);
+    
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "&" + "api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
+    // console.log(queryURL);
+    // Creating an AJAX call for the specific hero button being clicked
+    $.ajax({
+        url:queryURL, 
+        method: 'GET'
+    }).then(function(response) {
+        var results = response.data;
+        console.log(results);
+
+        $('#heroes-view').empty();
+
+            for(var i=0; i<results.length; i++) {
+            $('#heroes-view').append("<p>Rating: "+response.data[i].rating+"</p>");
+            $('#heroes-view').prepend("<img src='"+response.data[i].images.fixed_height.url+"'>");
+
+        // for (var i = 0; i < results.length; i++) {
+            // if (results[i].rating === "g" && results[i].rating === "pg-13") {
+
+    // //    // Retrieving the URL for the image
+    //    var imgURL = response.data[i].images.downsized.url;
+    // //    console.log(imgURL);              
+
+    // //     // Creating an element to hold the image
+    //     var image = $("<img>").attr("src", imgURL);
+    //     console.log(image);
+
+        // Creating a div to hold the gifs
+        var heroDiv = $("<div class='hero'>");
+
+        // Storing the rating data
+        var rating = results[i].rating;
+        
+        // Creating a paragraph tag with the result item's rating
+        var displayRating = $('<p>').text("Rating: " + rating);
+
+        // Creating an image tag
+        var heroImage = $("<img>");
+
+        // Giving the image tag an src attribute of a property pulled off the result item
+        heroImage.attr("src", results[i].images.fixed_height.url);
+        
+        // Appending the paragraph and personImage created to the "gifDiv" div created
+        heroDiv.append(p);
+        // Display the image
+        heroDiv.append(heroImage);
+
+        // Putting the entire GIF above the previous GIFs
+        // $("#heroes-view").prepend(heroDiv);
+            }
+        });
+    };
+
+
+// events
+$(document).on('click', '#add-hero', addHero);
+$(document).on('click', '.hero-btn', displayRatingInfo);
+$(document).on('click', '', function() {} );
+renderButtons();
+
+
+
+
 
         // FROM: BUTTON TRIGGERED VIDEO
 //  $('button').on('click', function(){
