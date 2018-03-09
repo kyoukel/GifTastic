3878f3743e841a66df914fc5ed7cdc5965342623
kyoukel <kimyoukel@gmail.com>
2018-03-08 17:37:45 -0800

    to get a better understanding of API's - I coded along with the lesson video to understand the queryUR L and the response.data.length for loop and what each do vs what I need them to do for this assignment. Now to find out  how to recreate this on the DOM using a for loop that loops through my array of super heroes and creates the buttons a nd also when buttons are clicked - grabs the giphy API by targeting the button values.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -1,4 +1,6 @@
 
+// Initial array of super heroes
+var movies = ["Wonder Woman", "Hulk", "Captain America", "Superman", "Thor", "Iron Man", "Spider-Man", "Guardians of the Galaxy", "Deadpool", "Batman", "X-Men", "Ant-Man", "Black Panther", "Suicide Squad", "Flash", "Supergirl", "Green Lantern", "Daredevil", "Elektra", "Autobots"];
 
 $('button').on('click', function(){
     // alert("Button Clicked!");
@@ -8,15 +10,34 @@ $('button').on('click', function(){
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + h + "&" + "api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
     console.log(queryURL);
 
+    // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
-        url:queryURL, method: 'GET'})
-        .done(function(response){
-            console.log(response);
-            for(var i=0; i<response.data.length; i++) {
-                $('#GIFArea').prepend("<p>Rating: "+response.data[i].rating+"</p>");
-                $('#GIFArea').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
-            }
+        url:queryURL, 
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response.data[0].rating);
 
-        })
+    // Creating a div to hold the gifs
+        var heroGIF = $("<div class='hero'>");
+
+    // Storing the rating data
+        var rating = response.data[0].rating;
+        console.log(rating);
+        
+    })
+        
+
+
 })
+        // FROM BUTTON TRIGGERED VIDEO
+        // .done(function(response){
+        //     console.log(response);
+        //     for(var i=0; i<response.data.length; i++) {
+        //         $('#GIFArea').prepend("<p>Rating: "+response.data[i].rating+"</p>");
+        //         $('#GIFArea').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
+        //     }
+
+
+
+
 
