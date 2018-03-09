c2e08e4827a0a7fd357b70b2bde168261fcd2d91
kyoukel <kimyoukel@gmail.com>
2018-03-08 17:32:44 -0800

    to get a better understanding of API's - I coded along with the lesson video to understand the queryURL and the response.data.length for loop and what each do vs what I need them to do for this assignment. Now to find out how to recreate this on the DOM using a for loop that loops through my array of super heroes and creates the buttons and also when buttons are clicked - grabs the giphy API by targeting the button values.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -12,6 +12,11 @@ $('button').on('click', function(){
         url:queryURL, method: 'GET'})
         .done(function(response){
             console.log(response);
+            for(var i=0; i<response.data.length; i++) {
+                $('#GIFArea').prepend("<p>Rating: "+response.data[i].rating+"</p>");
+                $('#GIFArea').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
+            }
+
         })
 })
 
