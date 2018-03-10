e0ce1468fbeb2bd4789ebd37e488bfdfe4dfed3a
kyoukel <kimyoukel@gmail.com>
2018-03-09 18:41:04 -0800

    still working on getting all 10 gifs to display when each button is clicked.

--- assets/gifTastic.js
+++ assets/gifTastic.js
@@ -27,8 +27,78 @@ function renderButtons() {
     }
 };    
 
+// Event listener for all button elements
+$('button').on('click', function() {
+    // "this" refers to a button that was clicked
+    var h = $(this).attr("data-");
+    // Constructing the URL to search Giphy for the name of the hero 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    h + "&api_key=S0s43WOAT4jzNdL4GICYdNrVfDl0MWMH&limit=10";
+    // Performing AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response){
+            console.log(response);
+        // Storing an array of results in the results variable
+        var results = response.data;
+            console.log(results);
+            //Looping over every result item.
+            for(var i=0; i<results.length; i++) {
+            // for(var i=0; i<results.length; i++) {
+        //         $('#heroes-view').append("<p>Rating: "+response.data[i].rating + "</p>" );
+        //         $('#heroes-view').prepend("<img src='"+results.fixed_height.url + " '>' " );
+
+                // Only taking action if the photo has an appropriate rating
+                if(results[i].rating !== "r" && results[i].rating !=="pg-13") {
+                    // Creating a div with the class "item"
+                    var gifDiv = $("<div class='item'>");
+                    // Storing the rating data
+                    var rating = results[i].rating;
+                    console.log(rating);
+                    // Creating a paragraph tag with the result item's rating
+                    var displayRating = $('<p>').text("Rating: " + rating);
+                    console.log(displayRating);
+                    // Creating an image tag
+                    var heroImage = $("<img>");
+                    // Giving the image tag an src attribute of a property pulled off the result item
+                    heroImage.attr("src", results[i].images.fixed_height.url);
+                    // Appending the paragraph and heroImage created to the "heroDiv" div created
+                    heroDiv.append(p);
+                    // Display the image
+                    heroDiv.append(heroImage);
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#heroes-view").prepend(heroDiv);
+                }
+        };
+    });    
+});
+
+
+// Creating a div to hold the gifs
+// var heroDiv = $("<div class='hero'>");
+
+// Storing the rating data
+// var rating = results[i].rating;
+
+// Creating a paragraph tag with the result item's rating
+// var displayRating = $('<p>').text("Rating: " + rating);
+
+// Creating an image tag
+// var heroImage = $("<img>");
+
+// Giving the image tag an src attribute of a property pulled off the result item
+// heroImage.attr("src", results[i].images.fixed_height.url);
+
+// Appending the paragraph and personImage created to the "gifDiv" div created
+// heroDiv.append(p);
+
+// Display the image
+// heroDiv.append(heroImage);
+
 // This function handles events when the submit button is clicked
-    function addHero(event) {
+$('#add-hero').on('click', function(event) {
+    // function addHero(event) {
     // $('#add-hero').on('click', function(event){
         // alert("Button Clicked!");
 
@@ -38,13 +108,14 @@ function renderButtons() {
 
         // This line will grab the text from the input box
         var hero = $("#heroes-input").val().trim();
-        console.log(hero);
+        // console.log(hero);
         // The superhero entered from the textbox is then added to our array
         superHeroes.push(hero);
 
         // calling renderButtons which handles the processing of our superHeroes array
         renderButtons();
-    }
+});
+
     
 // displayRatingInfo function re-renders the HTML to display the appropriate content
 function displayRatingInfo() {
@@ -71,13 +142,13 @@ function displayRatingInfo() {
         // for (var i = 0; i < results.length; i++) {
             // if (results[i].rating === "g" && results[i].rating === "pg-13") {
 
-    // //    // Retrieving the URL for the image
-    //    var imgURL = response.data[i].images.downsized.url;
-    // //    console.log(imgURL);              
+        // Retrieving the URL for the image
+        // var imgURL = response.data[i].images.downsized.url;
+        // console.log(imgURL);              
 
-    // //     // Creating an element to hold the image
-    //     var image = $("<img>").attr("src", imgURL);
-    //     console.log(image);
+        // Creating an element to hold the image
+        // var image = $("<img>").attr("src", imgURL);
+        // console.log(image);
 
         // Creating a div to hold the gifs
         var heroDiv = $("<div class='hero'>");
@@ -94,7 +165,7 @@ function displayRatingInfo() {
         // Giving the image tag an src attribute of a property pulled off the result item
         heroImage.attr("src", results[i].images.fixed_height.url);
         
-        // Appending the paragraph and personImage created to the "gifDiv" div created
+        // Appending the paragraph and personImage created to the "heroDiv" div created
         heroDiv.append(p);
         // Display the image
         heroDiv.append(heroImage);
@@ -107,9 +178,9 @@ function displayRatingInfo() {
 
 
 // events
-$(document).on('click', '#add-hero', addHero);
+// $(document).on('click', '#add-hero', addHero);
 $(document).on('click', '.hero-btn', displayRatingInfo);
-$(document).on('click', '', function() {} );
+// $(document).on('click', '', function() {} );
 renderButtons();
 
 
